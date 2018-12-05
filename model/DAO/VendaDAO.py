59bee934375863b130c49991dd0bfa3caa497e49
hebercrvg <heberreys@gmail.com>
2018-12-05 16:02:39 -0300

    Commit 05/12/2018

--- model/DAO/VendaDAO.py
+++ model/DAO/VendaDAO.py
@@ -16,10 +16,39 @@ class VendaDAO:
         conn.commit()
         conn.close()
 
+
+    def pesquisar_todos_devedores_por_divida():
+        conn = ConexaoBanco.get_conexao()
+        query = ("""SELECT V.CODVENDA, C.CODCLI, C.NOME,
+                 SUM(V.VALORVENDA-V.VALORPAGO) AS 'DIVIDA'
+                 FROM CLIENTE C
+                 INNER JOIN VENDA V
+                 ON C.CODCLI = V.CODCLI
+                 WHERE V.VALORPAGO < V.VALORVENDA 
+                 GROUP BY V.CODCLI
+                 ORDER BY C.NOME ASC""")
+        result = conn.execute(query)
+        return result
+
+    def pesquisar_devedor_por_divida(nome):
+        conn = ConexaoBanco.get_conexao()
+        query = ("SELECT V.CODVENDA, C.CODCLI, C.NOME, strftime('%d/%m/%Y', V.dtvenda),"
+                 "V.VALORVENDA, V.VALORPAGO, V.VALORVENDA-V.VALORPAGO AS 'DIVIDA' \n"
+                 "FROM CLIENTE C \n"
+                 "INNER JOIN VENDA V \n"
+                 "ON C.CODCLI = V.CODCLI \n"
+                 "WHERE V.VALORPAGO < V.VALORVENDA \n"
+                 "AND NOME LIKE '%{}%' \n"
+                 "AND C.CODCLI = V.CODCLI \n"
+                 "ORDER BY C.NOME ASC".format(nome))
+        result = conn.execute(query)
+        return result
+
+
     def pesquisar_todos_devedores():
         conn = ConexaoBanco.get_conexao()
-        query = ("""SELECT V.CODVENDA, C.CODCLI, C.NOME, strftime('%d/%m/%Y', V.dtvenda) AS 'DATA VENDA',
-                 V.VALORVENDA, V.VALORPAGO, SUM(V.VALORVENDA-V.VALORPAGO) AS 'DIVIDA'
+        query = ("""SELECT V.CODVENDA, C.CODCLI, C.NOME,
+                 SUM(V.VALORVENDA-V.VALORPAGO) AS 'DIVIDA'
                  FROM CLIENTE C
                  INNER JOIN VENDA V
                  ON C.CODCLI = V.CODCLI
