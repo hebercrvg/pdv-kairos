21ff0b2215e52e6e1bda7fdb2280b444692a1a5f
hebercrvg <heberreys@gmail.com>
2018-12-14 16:33:22 -0300

    Commit 14/12/2018

--- main.py
+++ main.py
@@ -1,10 +1,11 @@
-from view.FormPrincipal import *
+from PyQt5.QtWidgets import QDialog
+from view.FormLogin import *
 import sys
 
 if __name__ == '__main__':
     app = QApplication(sys.argv)
-    FormPrincipal = QMainWindow()
-    ui = Ui_FormPrincipal()
-    ui.setupUi(FormPrincipal)
-    FormPrincipal.show()
+    dialog = QDialog()
+    ui = Ui_FormLogin()
+    ui.setupUi(dialog)
+    dialog.show()
     sys.exit(app.exec_())
